9d928d38a2a33ab18695bd6aacac6655d80d4f91
bayartsetseg <bayartsetsegley@gmail.com>
2026-04-08 18:34:01 +0800

    fixed PORT for Rende5r

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,8 @@
-import app from './app.js';
-import { initDb } from './db/index.js';
-
-const PORT = process.env.BACKEND_PORT || 3001;
-
+import app from "./app.js";
+import { initDb } from "./db/index.js";
+const PORT = process.env.PORT || process.env.BACKEND_PORT || 3001;
 initDb().then(() => {
-  app.listen(PORT, '0.0.0.0', () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Backend server running on http://localhost:${PORT}`);
   });
 });
